b78dd85dfc21d5760825032be25a84b1bc8e7b2e
Shanley Rosero <shanleyrosero@gmail.com>
2021-01-13 04:31:20 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+fetch('https://api.thecatapi.com/v1/images/search')
+  .then(
+    function(response) {
+      if (response.status !== 200) {
+        console.log('Looks like there was a problem. Status Code: ' +
+          response.status);
+        return;
+      }
+
+      // Examine the text in the response
+      response.json().then(function(data) {
+        console.log(data);
+      });
+    }
+  )
+  .catch(function(err) {
+    console.log('Fetch Error :-S', err);
+  });
